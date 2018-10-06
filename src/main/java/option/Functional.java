8e54a2ada123f1b8439f5d0ecaa86cfdbcea5d91
Ryan Liao <irliao@ryans-air.home>
2018-10-05 20:29:23 -0700

    added src and pdf for Option tech talk

--- src/main/java/option/Functional.java
+++ src/main/java/option/Functional.java
@@ -1,5 +1,35 @@
 package option;
 
-public class Functional {
+import io.vavr.control.Option;
 
+class Functional {
+
+    // compiles and safe code since negative case was handled
+    Integer getLengthGood(String s) {
+        return Option.of(s).
+            map(nonNullS -> nonNullS.length()).
+            getOrElse(0); // assume 0 is our default value
+    }
+
+    // does not compile because negative case was not handled
+//    Integer getLengthBad(String s) {
+//        return Option.of(s).
+//            map(nonNullS -> nonNullS.length());
+//    }
+
+    // does not compile because no default value is provided
+//    Integer getLengthReallyBad(String s) {
+//        return Option.of(s).
+//            map(nonNullS -> nonNullS.length()).
+//            getOrElse();
+//    }
+
+    String isLengthEvenOrOdd(String s) {
+        return Option.of(s).
+            filter(nonNullS -> nonNullS.length() > 0).
+            map(nonNullS -> s.length()).
+            map(strInt -> strInt % 2 == 0 ? "Even" : "Odd").
+            getOrElse("Empty");
+    }
 }
+
--- src/main/java/option/Imperative.java
+++ src/main/java/option/Imperative.java
@@ -1,5 +1,42 @@
 package option;
 
-public class Imperative {
+class Imperative {
 
+    // compiles and negative case handled
+    Integer getLengthGood(String s) {
+        if (s == null) {
+            return 0; // assume 0 is our default value
+        }
+        return s.length();
+    }
+
+    // compiles and negative case unhandled
+    Integer getLengthBad(String s) {
+        return s.length(); // NPE
+    }
+
+    // compiles, null checked, but wrong
+    Integer getLengthReallyBad(String s) {
+        if (s == null) {
+            // some operation(s) unrelated to s
+        }
+        return s.length();
+    }
+
+    String isLengthEvenOrOdd(String s) {
+        if (s == null) {
+            return "Empty";
+        }
+
+        int length = s.length();
+        if (length > 0) {
+            if (length % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+
+        return "Empty";
+    }
 }
